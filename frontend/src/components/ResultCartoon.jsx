239871a15c23e24020ea7999331af27383e09474
VanillaSkys <filllovefamily@gmail.com>
2024-08-17 17:08:40 +0700

    Create ResultCartoon.jsx

---
+++ frontend/src/components/ResultCartoon.jsx
@@ -0,0 +1,10 @@
+function ResultCartoon() {
+  return (
+    <div className="flex justify-between w-full">
+        <h1 className="text-slate-700 text-lg font-medium">Result</h1>
+        <h2 className="text-slate-700 text-lg font-medium">3</h2>
+    </div>
+  )
+}
+
+export default ResultCartoon
