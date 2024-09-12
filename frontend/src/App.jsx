12f1741f6f7e7d95731ef3f9d56d98cdec13659f
HaMaGoD <boss.jilawat@gmail.com>
2024-09-12 22:30:32 +0700

    .

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,11 @@
-import { createContext, useEffect, useState } from "react";
-export const ContextCartoon = createContext();
 import Search from "./components/Search";
 import TypeSelect from "./components/TypeSelect";
 import ResultCartoon from "./components/ResultCartoon";
 import FilterAlphabet from "./components/FilterAlphabet";
 import PaginationContent from "./components/PaginationContent";
+import { createContext, useEffect, useState } from "react";
+
+export const ContextCartoon = createContext();
 
 function App() {
   const [res, setRes] = useState("");
@@ -12,12 +13,10 @@ function App() {
   const [showCartoons, setShowCartoons] = useState([]);
   const [error, setError] = useState(false);
 
-  const CACHE_TIME = 1000 * 60 * 60; // 1 ชั่วโมง
-
+  const CACHE_TIME = 1000 * 60 * 60; 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        // const response = await fetch("http://localhost:8000/");
         const response = await fetch("https://toc-app-be.onrender.com");
         const result = await response.json();
         setRes(result);
@@ -35,19 +34,15 @@ function App() {
         if (cachedData && cachedTime && Date.now() - cachedTime < CACHE_TIME) {
           setCartoons(JSON.parse(cachedData));
         } else {
-          const response = await fetch(
-            "https://toc-app-be.onrender.com/scrape"
-          );
-          // const response = await fetch("http://localhost:8000/scrape");
+          const response = await fetch("https://toc-app-be.onrender.com/scrape");
           if (!response.ok) {
             throw new Error("Network response was not ok");
           }
           const result = await response.json();
-          console.log(result);
           setCartoons(result);
           setShowCartoons(result);
           localStorage.setItem("cartoons", JSON.stringify(result));
-          localStorage.setItem("cartoonsTimestamp", Date.now()); // เก็บเวลาปัจจุบัน
+          localStorage.setItem("cartoonsTimestamp", Date.now()); 
         }
       } catch (error) {
         console.log(error);
@@ -58,12 +53,17 @@ function App() {
     fetchCartoons();
   }, []);
 
+
+  const handleSearch = (searchTerm) => {
+    const filteredCartoons = cartoons.filter((cartoon) =>
+      cartoon.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+    setShowCartoons(filteredCartoons);
+  };
+
   const downloadCSV = async () => {
     try {
-      // const response = await fetch('http://localhost:8000/download-csv');
-      const response = await fetch(
-        "https://toc-app-be.onrender.com/download-csv"
-      );
+      const response = await fetch("https://toc-app-be.onrender.com/download-csv");
       if (!response.ok) {
         throw new Error("Network response was not ok");
       }
@@ -72,7 +72,7 @@ function App() {
       const a = document.createElement("a");
       a.style.display = "none";
       a.href = url;
-      a.download = "manga_list.csv"; // The name of the file when it's downloaded
+      a.download = "manga_list.csv";
       document.body.appendChild(a);
       a.click();
       window.URL.revokeObjectURL(url);
@@ -80,11 +80,12 @@ function App() {
       console.error("Failed to download CSV:", error);
     }
   };
+
   return (
     <ContextCartoon.Provider
       value={{ cartoons, showCartoons, setShowCartoons }}
     >
-      <section className="text-center">
+      <section className="text-center ">
         {error ? (
           <>
             <p className="text-3xl font-bold text-red-500 my-6">
@@ -94,31 +95,34 @@ function App() {
           </>
         ) : res ? (
           <>
-          <div>
-
-            <h1 className="text-3xl font-bold text-cyan-400 my-6 tailwind-icon">
-              {res.message}
-            </h1>
-            <button
-              disabled={!(cartoons && cartoons.length >= 1)}
-              onClick={downloadCSV}
-              className={`my-6 rounded-full px-3 ${!(cartoons && cartoons.length >= 1) ? "bg-slate-500" : "bg-green-600 hover:bg-green-500"} `}
+            <div>
+              <h1 className="text-3xl font-bold text-cyan-400 my-6 tailwind-icon">
+                {res.message}
+              </h1>
+              <button
+                disabled={!(cartoons && cartoons.length >= 1)}
+                onClick={downloadCSV}
+                className={`my-6 rounded-full px-3 ${
+                  !(cartoons && cartoons.length >= 1)
+                    ? "bg-slate-500"
+                    : "bg-green-600 hover:bg-green-500"
+                } `}
               >
-              Export CSV
-            </button>
-              </div>
-            {/* cartoon display  */}
+                Export CSV
+              </button>
+            </div>
+            {/* cartoon display */}
             {cartoons && cartoons.length >= 1 ? (
               <section className="container max-w-5xl my-12 mx-auto">
                 <div className="flex gap-4 card">
                   <section className="flex flex-col gap-2">
-                    <Search />
+                    <Search onSearch={handleSearch} />
                     <TypeSelect />
                     <ResultCartoon />
                   </section>
                   <section>
                     <FilterAlphabet />
-                    <PaginationContent data={cartoons} />
+                    <PaginationContent data={showCartoons} />
                   </section>
                 </div>
               </section>
--- frontend/src/components/Search.jsx
+++ frontend/src/components/Search.jsx
@@ -1,18 +1,30 @@
+import { useState } from 'react';
 import { FaSearch } from 'react-icons/fa';
 
-function Search() {
+function Search({ onSearch }) {
+  const [searchTerm, setSearchTerm] = useState("");
+
+
+  const handleInputChange = (event) => {
+    const value = event.target.value;
+    setSearchTerm(value);
+    onSearch(value); 
+  };
+
   return (
     <div className="relative w-full max-w-xs">
       <input
         type="text"
+        value={searchTerm}
+        onChange={handleInputChange}
         className="w-full pl-10 pr-4 py-2 border border-none bg-indigo-100 rounded-full focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500"
         placeholder="Search..."
       />
       <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-        <FaSearch className="text-slate-500 " />
+        <FaSearch className="text-slate-500" />
       </div>
     </div>
-  )
+  );
 }
 
-export default Search
+export default Search;
